c4c19ff03bb91745e2c97dc7889f50263ba4239c
stephanieonyekaba <onyek009@umn.edu>
2024-10-26 17:04:49 -0500

    glow guide text and purple background added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,22 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet,Text, View } from 'react-native';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
+export default function App() {
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+      <View style= {styles.container}>
+        <Text> Glow Guide </Text>
+        <StatusBar style="auto" />
+      </View>
+
   );
 }
+const styles = StyleSheet.create ({
+  container: {
+    flex: 1,
+    backgroundColor: '#a9a1ff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+
+})
